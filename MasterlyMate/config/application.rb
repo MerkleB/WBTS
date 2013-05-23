380420b7ea0ea4372f594e654e255aa1e01d9818
JeyB88 <JeyB88@github.com>
2013-05-23 17:13:31 +0200

    set precompile assets initialization to false

--- MasterlyMate/config/application.rb
+++ MasterlyMate/config/application.rb
@@ -94,5 +94,7 @@ module MasterlyMate
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = false
   end
 end
